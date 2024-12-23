0930671ba4257e25cb15b28e80cdbc4c417b71e8
AmaliNur <amalnuritdinov5@gmail.com>
2024-12-23 20:46:24 +0300

    выполнен проект

---
+++ script/init.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Марракеш',
+    link: 'https://images.unsplash.com/photo-1555169048-ee590a06ab39?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1366&q=80'
+  },
+  {
+    name: 'Собор Святого Вита',
+    link: 'https://images.unsplash.com/photo-1509321395964-9173e84ffe76?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80'
+  },
+  {
+    name: 'Баухаус',
+    link: 'https://images.unsplash.com/photo-1522053484860-7d5e4955df6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=693&q=80'
+  },
+  {
+    name: 'Санторини',
+    link: 'https://images.unsplash.com/photo-1533105079780-92b9be482077?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+  },
+  {
+    name: 'Токио',
+    link: 'https://images.unsplash.com/photo-1503899036084-c55cdd92da26?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80'
+  },
+  {
+    name: 'Копенгаген',
+    link: 'https://images.unsplash.com/photo-1583705325385-b04be897ed7c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1963&q=80'
+  }
+];
