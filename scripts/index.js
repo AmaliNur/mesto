64e19bf2f9a042dd302c368987d36bd7a3012d35
AmaliNur <amalnuritdinov5@gmail.com>
2025-02-19 23:27:06 +0300

    добавлена функция создания карточки + вывод карточек массива

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,25 @@
 // @todo: Темплейт карточки
-
+const placesList = document.querySelector(".places__list");
 // @todo: DOM узлы
-
-// @todo: Функция создания карточки
 
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+function createCard(cardData) {
+    const cardTemplate = document.getElementById("card-template").content;
+    const cardElement = cardTemplate.querySelector(".places__item").cloneNode(true);
+    const cardDescription = cardElement.querySelector(".card__description");
+    const cardTitle = cardDescription.querySelector(".card__title");
+    const cardImage = cardElement.querySelector(".card__image");
+    
+    cardTitle.textContent = cardData.name;
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+
+    return cardElement;
+}
+
+initialCards.forEach((cardData) => {
+    const cardElement = createCard(cardData);
+    placesList.appendChild(cardElement);
+})
