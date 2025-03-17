d1af1f1aebd71b85cd4d1fe30ff6bb00cbf78c94
AmaliNur <amalnuritdinov5@gmail.com>
2025-03-17 13:15:30 +0300

    добавлено закрытие попапа на Escape

--- scripts/index.js
+++ scripts/index.js
@@ -65,10 +65,12 @@ initialCards.forEach((cardData) => {
 
 function openModal(popup) {
     popup.classList.add("popup_is-opened");
+    document.addEventListener("keydown", closeByEsc); 
 }
 
 function closeModal(popup) {
     popup.classList.remove("popup_is-opened");
+    document.removeEventListener("keydown", closeByEsc);
 }
 
 // Обработчик формы профиля
@@ -120,4 +122,12 @@ popups.forEach((popup) => {
         }
     })
 })
+
+// Закрытие попапа на кнопку Esc
+function closeByEsc(evt) {    
+     if (evt.key === "Escape") {     
+        const openedPopup = document.querySelector('.popup_is-opened');     
+        closeModal(openedPopup);   
+    } 
+}
 
